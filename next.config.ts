bdddb0b71c3982e9e5bc896700e96a47a57266ed
kingavatar <saikiranreddy06@gmail.com>
2025-01-29 02:13:52 +0530

    fix: basePath for images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: 'landing-site',
+  basePath: '/landing-site',
   images: {
     remotePatterns: [
       {
